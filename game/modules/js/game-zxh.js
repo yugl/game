041ac26a283372d428ebbb9d5e39f023290d9240
yugl <913605064@aa.com>
2016-11-19 17:34:47 +0800

    界面美化

--- game/modules/js/game-zxh.js
+++ game/modules/js/game-zxh.js
@@ -1,5 +1,6 @@
 var LocalKey = "Zxh"; 
 
+
 if( !getLocalStg( LocalKey ) ){ 
 	var _arry = getZxhData(); 
 	setLocalStg( LocalKey, _arry ); 
@@ -8,6 +9,21 @@ if( !getLocalStg( LocalKey ) ){
 var gamezj = GetRandomstrFromArry( getLocalStg( LocalKey ) );
 document.getElementById('textLeave').innerHTML = gamezj.getLength(); 
 
+if( gamezj.getLength() == 0 ){
+	clearLocalStg(); 
+};
+
+/*=============== 清理缓存*/
+function clearLocalStg(){
+	var _flag = confirm('您没有可供使用的问题了，请点确认获取数据！');
+
+	if( _flag ){
+		removeLocalStg( LocalKey );
+		location.reload( true );
+	};
+};
+
+/*=============== 获取随机产生的数据*/
 function getRandomVal(){ 
 
 	document.getElementById('word').value = gamezj.getRandomStr();  
@@ -16,7 +32,7 @@ function getRandomVal(){
 	setLocalStg( LocalKey, gamezj.getData() )
 }; 
 
-
+/*=============== 获取数据*/
 function getZxhData(){
 	var _data = ['555', '666', '777']; 
 	return _data;
